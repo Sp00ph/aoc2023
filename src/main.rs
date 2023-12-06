c1502de5619b226f0da877d91063c4209540ce77
Markus Everling <markuseverling@gmail.com>
2023-12-06 21:44:27 +0100

    More timings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(isqrt)]
 
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 use anyhow::Context;
 use clap::Parser;
@@ -37,15 +37,25 @@ enum Args {
         input: Option<String>,
         #[arg(short = 't', long)]
         show_time: bool,
+        #[arg(short = 'T', long)]
+        show_total_time: bool,
     },
     #[clap(alias = "ra")]
     RunAll {
         #[arg(short = 't', long)]
         show_time: bool,
+        #[arg(short = 'T', long)]
+        show_total_time: bool,
     },
 }
 
-fn run_part(day: usize, part: usize, input: Option<String>, show_time: bool) -> anyhow::Result<()> {
+fn run_part(
+    day: usize,
+    part: usize,
+    input: Option<String>,
+    show_time: bool,
+    acc: Option<&mut Duration>,
+) -> anyhow::Result<()> {
     let input = match input {
         Some(input) => input,
         None => std::fs::read_to_string(format!("input/day{}.txt", day))
@@ -60,6 +70,9 @@ fn run_part(day: usize, part: usize, input: Option<String>, show_time: bool) -> 
     if show_time {
         println!("Finished in: {:.3?}", elapsed);
     }
+    if let Some(acc) = acc {
+        *acc += elapsed;
+    }
     Ok(())
 }
 
@@ -71,19 +84,35 @@ fn main() -> anyhow::Result<()> {
             part,
             input,
             show_time,
-        } => run_part(day, part, input, show_time),
+        } => run_part(day, part, input, show_time, None),
         Args::RunDay {
             day,
             input,
             show_time,
+            show_total_time,
         } => {
-            run_part(day, 1, input.clone(), show_time)?;
-            run_part(day, 2, input, show_time)
+            let mut acc = show_total_time.then(|| Duration::ZERO);
+
+            run_part(day, 1, input.clone(), show_time, acc.as_mut())?;
+            run_part(day, 2, input, show_time, acc.as_mut())?;
+
+            if let Some(acc) = acc {
+                println!("Total time: {:.3?}", acc);
+            }
+            Ok(())
         }
-        Args::RunAll { show_time } => {
+        Args::RunAll {
+            show_time,
+            show_total_time,
+        } => {
+            let mut acc = show_total_time.then(|| Duration::ZERO);
             for day in 1..=25 {
-                run_part(day, 1, None, show_time)?;
-                run_part(day, 2, None, show_time)?;
+                run_part(day, 1, None, show_time, acc.as_mut())?;
+                run_part(day, 2, None, show_time, acc.as_mut())?;
+            }
+
+            if let Some(acc) = acc {
+                println!("Total time: {:.3?}", acc);
             }
             Ok(())
         }
