8a7ef78ba3a74d704ef23e0f3084ccf7b80ae89f
Markus Everling <markuseverling@gmail.com>
2023-12-12 13:03:30 +0100

    Remove debug formatting, add comments.

--- src/day12.rs
+++ src/day12.rs
@@ -1,8 +1,6 @@
-use std::fmt;
-
 use ahash::AHashMap;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+#[derive(PartialEq, Eq, Clone, Copy, Hash)]
 enum SpringStatus {
     Working,
     Broken,
@@ -12,31 +10,6 @@ enum SpringStatus {
 struct Row {
     springs: Vec<SpringStatus>,
     blocks: Vec<usize>,
-}
-
-impl fmt::Debug for Row {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", row_to_string(&self.springs, &self.blocks))
-    }
-}
-
-fn row_to_string(springs: &[SpringStatus], blocks: &[usize]) -> String {
-    let springs = springs
-        .iter()
-        .map(|s| match s {
-            SpringStatus::Working => '.',
-            SpringStatus::Broken => '#',
-            SpringStatus::Unknown => '?',
-        })
-        .collect::<String>();
-
-    let blocks = blocks
-        .iter()
-        .map(|b| b.to_string())
-        .collect::<Vec<_>>()
-        .join(",");
-
-    format!("{} {}", springs, blocks)
 }
 
 fn parse_row(line: &str) -> Row {
@@ -64,6 +37,9 @@ fn parse_input(input: &str) -> Vec<Row> {
 type Cache<'a> = AHashMap<(&'a [SpringStatus], &'a [usize]), usize>;
 
 fn count_arrangements<'a>(row: &'a Row, cache: &mut Cache<'a>) -> usize {
+    ///  Tries to remove the first `n` springs from the front of the slice, and returns `None`
+    ///  if any of them are working or if the slice is too short. Otherwise,
+    ///  returns the rest of the slice.
     fn munch_not_working(springs: &[SpringStatus], n: usize) -> Option<&[SpringStatus]> {
         if springs.len() < n {
             return None;
@@ -75,8 +51,8 @@ fn count_arrangements<'a>(row: &'a Row, cache: &mut Cache<'a>) -> usize {
         Some(rest)
     }
 
-    // wrapper that checks the cache and returns early if it's a cache hit.
-    // if it's a cache miss, it computes the value and inserts it into the cache
+    /// Wrapper that checks the cache and returns early if it's a cache hit.
+    /// If it's a cache miss, it computes the value and inserts it into the cache
     fn rec<'a>(springs: &'a [SpringStatus], blocks: &'a [usize], cache: &mut Cache<'a>) -> usize {
         if let Some(&n) = cache.get(&(springs, blocks)) {
             return n;
@@ -92,6 +68,8 @@ fn count_arrangements<'a>(row: &'a Row, cache: &mut Cache<'a>) -> usize {
         blocks: &'a [usize],
         cache: &mut Cache<'a>,
     ) -> usize {
+        // If the blocks require more broken springs than there are total springs,
+        // there are no arrangements.
         if springs.len() + 1 < blocks.iter().sum::<usize>() + blocks.len() {
             return 0;
         }
@@ -101,34 +79,46 @@ fn count_arrangements<'a>(row: &'a Row, cache: &mut Cache<'a>) -> usize {
             match springs {
                 [SpringStatus::Working, rest @ ..] => springs = rest,
                 [head, rest @ ..] => break (*head, rest),
+                // If there are no non-working springs, then there is only
+                // an arrangement if there are no blocks.
                 [] => return usize::from(blocks.is_empty()),
             }
         };
 
+        // If there are no blocks, then there is only an arrangement if no springs are broken.
         if blocks.is_empty() {
             return springs.iter().all(|s| *s != SpringStatus::Broken) as usize;
         }
 
+        // If the first spring is broken, then we can immediately munch the entire first block.
         if head == SpringStatus::Broken {
             let n = blocks[0];
             let Some(rest) = munch_not_working(rest, n - 1) else {
                 return 0;
             };
             let rest = match rest {
+                // If there are no remaining springs, then there is only an
+                // arrangement if there are also no remaining blocks.
                 [] => return usize::from(blocks[1..].is_empty()),
+                // If the block of broken springs is followed by a broken spring,
+                // then there are no arrangements.
                 [SpringStatus::Broken, ..] => return 0,
+                // Otherwise, skip the first spring (it cannot be broken) as well as
+                // the first block, and count the remaining arrangements.
                 [_, rest @ ..] => rest,
             };
 
             return rec(rest, &blocks[1..], cache);
         }
 
-        // head == SpringStatus::Unknown
+        // At this point we know that head == SpringStatus::Unknown
 
-        // count possibilities where the spring is working
+        // count possibilities where the spring is working.
         let arrangements_if_working = rec(rest, blocks, cache);
 
-        // count possibilities where the spring is broken
+        // count possibilities where the spring is broken.
+        // If the first block only has one spring, just check it directly
+        // and recurse. Otherwise, munch the whole block and recurse.
         let arrangements_if_broken = if blocks[0] == 1 {
             let rest = match rest {
                 [] => return arrangements_if_working + usize::from(blocks[1..].is_empty()),
