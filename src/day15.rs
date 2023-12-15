85a918704a00ab0e85a888a68f8be700c6e44fb2
Markus Everling <markuseverling@gmail.com>
2023-12-15 11:19:30 +0100

    Optimize Day 15 a bit

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,7 @@
-use linked_hash_map::{Entry, LinkedHashMap};
-
 fn hash(bytes: &[u8]) -> u8 {
-    bytes.iter().fold(0u8, |acc, &b| acc.wrapping_add(b).wrapping_mul(17))
+    bytes
+        .iter()
+        .fold(0u8, |acc, &b| acc.wrapping_add(b).wrapping_mul(17))
 }
 
 fn lenses(input: &str) -> impl Iterator<Item = &str> {
@@ -9,43 +9,45 @@ fn lenses(input: &str) -> impl Iterator<Item = &str> {
 }
 
 pub fn part1(input: &str) -> String {
-    lenses(input).map(|s| hash(s.as_bytes()) as usize).sum::<usize>().to_string()
+    lenses(input)
+        .map(|s| hash(s.as_bytes()) as usize)
+        .sum::<usize>()
+        .to_string()
 }
 
 pub fn part2(input: &str) -> String {
-    // We use `LinkedHashMap` because it keeps track of insertion order,
-    // but allows efficient removal of elements in the middle of the list.
-    // `Vec` would require O(n) to find and remove a given box, whereas
-    // `LinkedHashMap` does it in O(1).
-    let mut boxes: [LinkedHashMap<&str, usize>; 256] =
-        std::array::from_fn(|_| LinkedHashMap::new());
+    // Asymptotically, this solution is not very efficient, as scanning a Vec or removing an
+    // element is O(n), whereas with something like a linked hash map, it would be O(1). However,
+    // the lists stay short enough that using a vector is over 2x faster than a linked hash map
+    // for my input.
+    let mut boxes: [Vec<(&str, usize)>; 256] = std::array::from_fn(|_| Vec::new());
     for lens in lenses(input) {
         if let Some(name) = lens.strip_suffix('-') {
             let hash = hash(name.as_bytes());
-            boxes[hash as usize].remove(name);
+            let lensbox = &mut boxes[hash as usize];
+            
+            // Remove the lens from the box if it's in there.
+            if let Some(idx) = lensbox.iter().position(|(n, _)| n == &name) {
+                lensbox.remove(idx);
+            }
         } else {
             let (name, focal_length) = lens.split_once('=').unwrap();
             let focal_length = focal_length.parse().unwrap();
             let hash = hash(name.as_bytes());
             let lensbox = &mut boxes[hash as usize];
 
-            // we can't just use `lensbox.insert()`, because
-            // that would update the lenses position to the
-            // end of the map.
-            match lensbox.entry(name) {
-                Entry::Occupied(mut o) => {
-                    *o.get_mut() = focal_length;
-                }
-                Entry::Vacant(v) => {
-                    v.insert(focal_length);
-                }
+            // If the lens is already in the box, replace it. Otherwise, add it.
+            if let Some((_, existing_focal_length)) = lensbox.iter_mut().find(|(n, _)| n == &name) {
+                *existing_focal_length = focal_length;
+            } else {
+                lensbox.push((name, focal_length));
             }
         }
     }
 
     let mut focusing_power = 0;
     for (box_idx, lensbox) in boxes.iter().enumerate() {
-        for (lens_idx, focal_length) in lensbox.values().enumerate() {
+        for (lens_idx, (_, focal_length)) in lensbox.iter().enumerate() {
             focusing_power += (box_idx + 1) * (lens_idx + 1) * focal_length
         }
     }
