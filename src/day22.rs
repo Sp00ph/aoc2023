8427c2dc20a6db0ca697b32f432ac8ca5d4944fb
Markus Everling <markuseverling@gmail.com>
2023-12-22 16:45:36 +0100

    Day 22 reimplement everything

--- src/day22.rs
+++ src/day22.rs
@@ -1,93 +1,23 @@
-use std::{collections::VecDeque, fmt};
-
-use ahash::AHashSet;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Axis {
-    X,
-    Y,
-    Z,
-}
+use smallvec::SmallVec;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 struct Brick {
     start: (u16, u16, u16),
-    len: u16,
-    axis: Axis,
-}
-
-impl fmt::Debug for Brick {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let (x, y, z) = self.start;
-        let (x2, y2, z2) = match self.axis {
-            Axis::X => (x + self.len, y, z),
-            Axis::Y => (x, y + self.len, z),
-            Axis::Z => (x, y, z + self.len),
-        };
-        write!(f, "({},{},{})~({},{},{})", x, y, z, x2, y2, z2)
-    }
+    end: (u16, u16, u16),
 }
 
 impl Brick {
     fn from_start_end(start: (u16, u16, u16), end: (u16, u16, u16)) -> Self {
         let (x1, y1, z1) = start;
         let (x2, y2, z2) = end;
-        let axis = if x1 != x2 {
-            Axis::X
-        } else if y1 != y2 {
-            Axis::Y
-        } else {
-            Axis::Z
-        };
-        let len = match axis {
-            Axis::X => x2.abs_diff(x1),
-            Axis::Y => y2.abs_diff(y1),
-            Axis::Z => z2.abs_diff(z1),
-        };
-        let start = (u16::min(x1, x2), u16::min(y1, y2), u16::min(z1, z2));
-        Self { start, len, axis }
-    }
-
-    fn xy_overlap(self, other: Self) -> bool {
-        let (x1, y1, _) = self.start;
-        let (x2, y2) = match self.axis {
-            Axis::X => (x1 + self.len, y1),
-            Axis::Y => (x1, y1 + self.len),
-            Axis::Z => (x1, y1),
-        };
-        let (x3, y3, _) = other.start;
-        let (x4, y4) = match other.axis {
-            Axis::X => (x3 + other.len, y3),
-            Axis::Y => (x3, y3 + other.len),
-            Axis::Z => (x3, y3),
-        };
-        let x_overlap = x3 <= x2 && x1 <= x4;
-        let y_overlap = y3 <= y2 && y1 <= y4;
-        x_overlap && y_overlap
-    }
-
-    fn overlaps(&self, other: Self) -> bool {
-        let (x1, y1, z1) = self.start;
-        let (x2, y2, z2) = match self.axis {
-            Axis::X => (x1 + self.len, y1, z1),
-            Axis::Y => (x1, y1 + self.len, z1),
-            Axis::Z => (x1, y1, z1 + self.len),
-        };
-        let (x3, y3, z3) = other.start;
-        let (x4, y4, z4) = match other.axis {
-            Axis::X => (x3 + other.len, y3, z3),
-            Axis::Y => (x3, y3 + other.len, z3),
-            Axis::Z => (x3, y3, z3 + other.len),
-        };
-        let x_overlap = x3 <= x2 && x1 <= x4;
-        let y_overlap = y3 <= y2 && y1 <= y4;
-        let z_overlap = z3 <= z2 && z1 <= z4;
-        x_overlap && y_overlap && z_overlap
+        let start = (x1.min(x2), y1.min(y2), z1.min(z2));
+        let end = (x1.max(x2), y1.max(y2), z1.max(z2));
+        Self { start, end }
     }
 }
 
 fn parse_input(input: &str) -> Vec<Brick> {
-    input
+    let mut bricks: Vec<_> = input
         .trim()
         .lines()
         .map(|line| {
@@ -100,106 +30,145 @@ fn parse_input(input: &str) -> Vec<Brick> {
             let end = (ex.parse().unwrap(), ey.parse().unwrap(), ez.parse().unwrap());
             Brick::from_start_end(start, end)
         })
-        .collect()
+        .collect();
+    bricks.sort_unstable_by_key(|brick| brick.start.2);
+    bricks
 }
 
-fn below(bricks: &[Brick]) -> Vec<Vec<usize>> {
-    let mut below: Vec<_> = (0..bricks.len()).map(|_| vec![]).collect();
-    // for each brick, find the bricks that are above and below it
-    for (i, brick) in bricks.iter().enumerate() {
-        for (j, other) in bricks.iter().enumerate() {
-            if i == j || !brick.xy_overlap(*other) {
+fn xy_limits(bricks: &[Brick]) -> ((u16, u16), (u16, u16)) {
+    let (mut x_min, mut x_max) = (u16::MAX, 0);
+    let (mut y_min, mut y_max) = (u16::MAX, 0);
+    for brick in bricks {
+        (x_min, x_max) = (x_min.min(brick.start.0), x_max.max(brick.end.0));
+        (y_min, y_max) = (y_min.min(brick.start.1), y_max.max(brick.end.1));
+    }
+    ((x_min, x_max), (y_min, y_max))
+}
+
+struct State<'a> {
+    bricks: &'a mut [Brick],
+    x_lims: (u16, u16),
+    y_lims: (u16, u16),
+    touching_above: Vec<SmallVec<[u16; 4]>>,
+    touching_below: Vec<SmallVec<[u16; 4]>>,
+}
+
+fn fall(state: &mut State) {
+    let width = (state.x_lims.1 - state.x_lims.0 + 1) as usize;
+    let height = (state.y_lims.1 - state.y_lims.0 + 1) as usize;
+
+    let mut grid = vec![usize::MAX; width * height];
+    let grid_idx = |x: u16, y: u16| {
+        (y as usize - state.y_lims.0 as usize) * width + (x as usize - state.x_lims.0 as usize)
+    };
+
+    for brick_idx in 0..state.bricks.len() {
+        let mut max_z = 0;
+        // first, do a pass to find the maximum z of any brick below
+        // the current one. the current brick will then be one above
+        // that maximum z.
+        let brick = state.bricks[brick_idx];
+        for y in brick.start.1..=brick.end.1 {
+            for x in brick.start.0..=brick.end.0 {
+                let below_idx = grid[grid_idx(x, y)];
+                if below_idx != usize::MAX {
+                    let top_of_below = state.bricks[below_idx].end.2;
+                    max_z = max_z.max(top_of_below);
+                }
+            }
+        }
+        let offset = brick.start.2 - max_z - 1;
+        state.bricks[brick_idx].start.2 -= offset;
+        state.bricks[brick_idx].end.2 -= offset;
+        // in the second pass, compute all the bricks that now
+        // touch the current one.
+        let brick = state.bricks[brick_idx];
+        for y in brick.start.1..=brick.end.1 {
+            for x in brick.start.0..=brick.end.0 {
+                let grid_idx = grid_idx(x, y);
+                let below_idx = grid[grid_idx];
+                if below_idx != usize::MAX {
+                    let top_of_below = state.bricks[below_idx].end.2;
+                    if top_of_below == max_z
+                        && !state.touching_below[brick_idx].contains(&(below_idx as u16))
+                    {
+                        state.touching_below[brick_idx].push(below_idx as u16);
+                        state.touching_above[below_idx].push(brick_idx as u16);
+                    }
+                }
+                grid[grid_idx] = brick_idx;
+            }
+        }
+    }
+}
+
+// counts the number of bricks that, if removed, would lead
+// to other bricks falling down.
+fn count_loadbearing(state: &State) -> usize {
+    let n = state.bricks.len();
+    let mut loadbearing = vec![false; n];
+    for below in &state.touching_below {
+        if below.len() == 1 {
+            loadbearing[below[0] as usize] = true;
+        }
+    }
+    loadbearing.iter().filter(|&&bit| bit).count()
+}
+
+fn sum_of_falling(state: &State) -> usize {
+    let n = state.bricks.len();
+    let mut falling = vec![false; n];
+    let mut sum = 0;
+    for piece_idx in 0..n {
+        falling.fill(false);
+        falling[piece_idx] = true;
+        'outer: for falling_idx in piece_idx + 1..n {
+            // in this case the piece is already on the bottom layer.
+            if state.touching_below[falling_idx].is_empty() {
                 continue;
             }
-            if i > j {
-                below[i].push(j);
+            // if there's any piece below that isn't falling, then
+            // the current piece isn't falling either.
+            for &below_idx in &state.touching_below[falling_idx] {
+                if !falling[below_idx as usize] {
+                    continue 'outer;
+                }
             }
-        }
-    }
-    below
-}
-
-fn fall(bricks: &mut [Brick], below: &[Vec<usize>]) {
-    for i in 0..bricks.len() {
-        loop {
-            let mut copy = bricks[i];
-            copy.start.2 -= 1;
-            let is_valid = copy.start.2 > 0 && below[i].iter().all(|&j| !copy.overlaps(bricks[j]));
-            if !is_valid {
-                break;
-            }
-            bricks[i] = copy;
-        }
-    }
-}
-
-fn supporting_and_supported_by(
-    bricks: &[Brick],
-    below: &[Vec<usize>],
-) -> (Vec<Vec<usize>>, Vec<Vec<usize>>) {
-    let mut supporting: Vec<_> = (0..bricks.len()).map(|_| vec![]).collect();
-    let mut supported_by: Vec<_> = (0..bricks.len()).map(|_| vec![]).collect();
-
-    for (i, &brick) in bricks.iter().enumerate() {
-        for &j in &below[i] {
-            let below = bricks[j];
-            let top_of_below =
-                if below.axis == Axis::Z { below.start.2 + below.len } else { below.start.2 };
-            if top_of_below == brick.start.2 - 1 {
-                supporting[j].push(i);
-                supported_by[i].push(j);
-            }
+            falling[falling_idx] = true;
+            sum += 1;
         }
     }
 
-    (supporting, supported_by)
+    sum
 }
 
 pub fn part1(input: &str) -> String {
-    let mut bricks = parse_input(input);
-    bricks.sort_unstable_by_key(|brick| brick.start.2);
-    let below = below(&bricks);
-    fall(&mut bricks, &below);
-    let (_, supported_by) = supporting_and_supported_by(&bricks, &below);
+    let bricks = parse_input(input);
+    let (x_lims, y_lims) = xy_limits(&bricks);
+    let mut state = State {
+        bricks: &mut bricks.clone(),
+        x_lims,
+        y_lims,
+        touching_above: vec![SmallVec::new(); bricks.len()],
+        touching_below: vec![SmallVec::new(); bricks.len()],
+    };
+    fall(&mut state);
 
-    let mut lone_supporters = AHashSet::new();
-    for supported_by in supported_by.iter() {
-        if supported_by.len() == 1 {
-            lone_supporters.insert(supported_by[0]);
-        }
-    }
-
-    (bricks.len() - lone_supporters.len()).to_string()
-}
-
-fn count_falling_if_removed(
-    supporting: &[Vec<usize>],
-    supported_by: &[Vec<usize>],
-    i: usize,
-) -> usize {
-    let mut removed = AHashSet::from([i]);
-    let mut queue = VecDeque::from([i]);
-    while let Some(brick_idx) = queue.pop_front() {
-        for &above in &supporting[brick_idx] {
-            if supported_by[above].iter().all(|&j| removed.contains(&j)) {
-                removed.insert(above);
-                queue.push_back(above);
-            }
-        }
-    }
-    removed.len() - 1
+    let non_loadbearing = state.bricks.len() - count_loadbearing(&state);
+    non_loadbearing.to_string()
 }
 
 pub fn part2(input: &str) -> String {
-    let mut bricks = parse_input(input);
-    bricks.sort_unstable_by_key(|brick| brick.start.2);
-    let below = below(&bricks);
-    fall(&mut bricks, &below);
-    let (supporting, supported_by) = supporting_and_supported_by(&bricks, &below);
+    let bricks = parse_input(input);
+    let (x_lims, y_lims) = xy_limits(&bricks);
+    let mut state = State {
+        bricks: &mut bricks.clone(),
+        x_lims,
+        y_lims,
+        touching_above: vec![SmallVec::new(); bricks.len()],
+        touching_below: vec![SmallVec::new(); bricks.len()],
+    };
+    fall(&mut state);
 
-    let mut total = 0usize;
-    for i in 0..bricks.len() {
-        total += count_falling_if_removed(&supporting, &supported_by, i);
-    }
-    total.to_string()
+    sum_of_falling(&state).to_string()
 }
